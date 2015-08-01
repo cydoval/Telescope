0cc2a3bae46bdbd7fe1e57868b7c5dec3acb3c06
Cal <cydoval@mac.com>
2015-08-01 10:26:17 -0700

    setting up my sass organization

---
+++ packages/telescope-theme-cal/package.js
@@ -0,0 +1,32 @@
+Package.describe({
+  summary: "Telescope Cal theme",
+  version: '0.1.0',
+  name: "telescope-theme-cal"
+});
+
+Package.onUse(function (api) {
+
+  api.use(['fourseven:scss', 'telescope:theme-hubble'], ['client']);
+
+  api.addFiles([
+    	 // modules
+      'lib/client/scss/modules/_accounts.scss',
+      'lib/client/scss/modules/_comments.scss',
+      'lib/client/scss/modules/_dialogs.scss',
+      'lib/client/scss/modules/_nav.scss',
+      'lib/client/scss/modules/_posts.scss',
+      'lib/client/scss/modules/_user-profile.scss',
+
+      // partials
+      'lib/client/scss/partials/_colors.scss',
+      'lib/client/scss/partials/_grid.scss',
+      'lib/client/scss/partials/_mixins.scss',
+      'lib/client/scss/partials/_tooltips.scss',
+      'lib/client/scss/partials/_typography.scss',
+
+      // screen
+      'lib/client/scss/screen.scss'
+
+    ], ['client']);
+
+});
